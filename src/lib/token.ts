97a657e10825d4728ecdd9558eaf55ba7047d6ad
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 20:59:27 +0700

    ⚡️ token.ts: created a function to generate a jwt token

---
+++ src/lib/token.ts
@@ -0,0 +1,10 @@
+import config from '@config'
+import jwt from 'jsonwebtoken'
+
+export const generateToken = (data: any) => {
+  const token = jwt.sign(data, config.jwtSecret, { 
+    // algorithm: 'RS256'
+    expiresIn: '1d'
+  })
+  return token
+}
