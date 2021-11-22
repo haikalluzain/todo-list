d0c2109a60d1a2642c324dd779ebb948845aba19
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 20:56:54 +0700

    ⚡️ logger.ts: created an instance for logging

---
+++ src/lib/logger.ts
@@ -0,0 +1,48 @@
+import winston from 'winston'
+import config from '@config'
+
+const transports: Array<any> = []
+if (process.env.NODE_ENV !== config.app.stage.development) {
+  transports.push(
+    new winston.transports.Console()
+  )
+} else {
+  transports.push(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.cli(),
+        winston.format.splat(),
+      )
+    })
+  )
+}
+
+const myFormatter = winston.format.printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+});
+
+transports.push(
+  new winston.transports.File({
+    filename: "logs/server.log"
+  }),
+  new winston.transports.File({
+    filename: "logs/error.log",
+    level: "error"
+  })
+)
+
+const LoggerInstance = winston.createLogger({
+  level: config.logs.level,
+  levels: winston.config.npm.levels,
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    myFormatter,
+    // winston.format.errors({stack: true}),
+    // winston.format.simple(),
+  ),
+  transports
+})
+
+export default LoggerInstance
