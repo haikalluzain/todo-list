3bc34fb94049a9998bbefd7beac168b93db65cd9
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 21:14:42 +0700

    🚚 user.ts: created user router

---
+++ src/api/routes/user.ts
@@ -0,0 +1,12 @@
+import userController from '@api/controllers/userController'
+import authMiddleware from '@api/middlewares/authMiddleware'
+import { Router } from 'express'
+
+const router = Router()
+
+router
+.route('/')
+.get(authMiddleware, userController.getUser)
+
+const userRoute = router
+export default userRoute
