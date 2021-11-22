f478e5aaf1832556e72465c256381ca07d2f0f4f
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 21:01:44 +0700

    ✨ response.ts: created an utility to send success or error response

---
+++ src/utils/response.ts
@@ -0,0 +1,61 @@
+import { Response } from 'express'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+
+/**
+ * Success Response
+ * @param res 
+ * @param message 
+ * @param data 
+ * @param statusCode 
+ */
+export const successResponse = (res: Response, message: string, data: any, statusCode: number = StatusCodes.OK) => {
+  res.status(statusCode).json({
+    statusCode,
+    message,
+    data
+  })
+}
+
+/**
+ * Error responses
+ */
+export const responseInternalServerError = (
+  res: Response,
+  message: string = ReasonPhrases.INTERNAL_SERVER_ERROR
+) => {
+  return errorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, message)
+}
+
+export const responseNotFound = (
+  res: Response,
+  message: string = ReasonPhrases.NOT_FOUND
+) => {
+  return errorResponse(res, StatusCodes.NOT_FOUND, message)
+}
+
+export const responseUnauthorized = (res: Response) => {
+  return errorResponse(res, StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+}
+
+export const responseUnprocessable = (
+  res: Response,
+  message: string = ReasonPhrases.UNPROCESSABLE_ENTITY
+) => {
+  return errorResponse(res, StatusCodes.UNPROCESSABLE_ENTITY, message)
+}
+
+export const responseBadRequest = (
+  res: Response,
+  message: string = ReasonPhrases.BAD_REQUEST
+) => {
+  return errorResponse(res, StatusCodes.BAD_REQUEST, message)
+}
+
+export const errorResponse = (res: Response, statusCode: number, message: string) => {
+  res.status(statusCode).json({
+    error: {
+      statusCode,
+      message
+    }
+  })
+}
