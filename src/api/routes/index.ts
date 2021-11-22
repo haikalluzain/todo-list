97e0f116e408372fe759b34757931f623885e534
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 21:15:18 +0700

    📝 index.ts: created a main file for routes

---
+++ src/api/routes/index.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import authRoute from './auth'
+import userRoute from './user'
+
+const router = Router()
+
+interface routeInterface {
+  path: string
+  route: Router
+}
+
+const routes: routeInterface[] = [
+  {
+    path: '/auth',
+    route: authRoute
+  },
+  {
+    path: '/users',
+    route: userRoute
+  }
+]
+
+routes.forEach((route) => {
+  router.use(route.path, route.route)
+})
+
+export default router
