707abb860fa6c8e4fe2845a2c3e550b09ac06bf6
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-22 21:16:04 +0700

    🚀 express.ts: configure the express

---
+++ src/lib/express.ts
@@ -0,0 +1,44 @@
+import { errorResponse, responseNotFound } from '@utils/response';
+import Logger from './logger';
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import config from '@config';
+import routes from '@api/routes';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes'
+
+export default ({ app }: { app: express.Application }) => {
+  /**
+   * Useful if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+   * It shows the real origin IP in the heroku or Cloudwatch logs
+   */
+  app.enable('trust proxy');
+
+  /**
+   * The magic package that prevents frontend developers going nuts
+   * Alternate description:
+   * Enable Cross Origin Resource Sharing to all origins by default
+   */
+  app.use(cors());
+
+  // Transforms the raw string of req.body into json
+  app.use(express.json());
+
+  // Load API routes
+  app.use(config.api.prefix, routes);
+
+  /// catch not found route (404) and forward to error handler
+  app.use((req: Request, res: Response) => {
+    responseNotFound(res)
+  });
+
+  /**
+   * All the error response (that not expected) will generated from here
+   * Also we logs all the message to the error.log file
+   */
+  app.use((err: { status: number, message: string, stack: any }, req: Request, res: Response) => {
+    Logger.error(err.stack)
+    const status = err.status || StatusCodes.INTERNAL_SERVER_ERROR
+    let message = getReasonPhrase(status)
+    errorResponse(res, status, message)
+  });
+}
