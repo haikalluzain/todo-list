bbf7e0dae92c80349014c792e45d6dcd1dc71429
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-28 11:51:07 +0700

    🔨 userController.ts: updated user controller

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -1,8 +1,9 @@
+import { RequestWithAuth } from "@api/middlewares/authMiddleware";
 import { successResponse } from "@utils/response";
 import { NextFunction, Request, Response } from "express";
 
 
-const getUser = async (req: Request, res: Response, next: NextFunction) => {
+const getUser = async (req: RequestWithAuth, res: Response, next: NextFunction) => {
   try {
     return successResponse(res, "Get the user data", req.user)
   } catch (error) {
