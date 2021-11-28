c7398800e05d19a3a1de51210671b9763f4d9698
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-28 11:25:49 +0700

    ♻️ app.ts: refactored the app file

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,44 @@
 import express from "express";
 import config from './config';
 import Logger from './lib/logger';
-
-async function startServer() {
-  const app = express()
+import expressLoader from './lib/express'
+import mongooseLoader from './lib/mongoose'
   
-  /**
-   * A little hack here
-   * Import/Export can only be used in 'top-level code'
-   * Well, at least in node 10 without babel and at the time of writing
-   * So we are using good old require.
-   **/
-   await require('./lib').default({ expressApp: app });
+const app = express()
 
-   app.listen(config.port, () => {
-     Logger.info(`
-     ####################################
-     #  Server listening on port: ${config.port}  #
-     ####################################
-     `)
-   }).on('error', err => {
-     Logger.error(err)
-     process.exit(1)
-   })
+async function startServer(app: express.Application) {
+  try {
+    
+    /**
+     * A little hack here
+     * Import/Export can only be used in 'top-level code'
+     * Well, at least in node 10 without babel and at the time of writing
+     * So we are using good old require.
+     **/
+    // await require('./lib').default({ expressApp: app });
+
+    expressLoader(app);
+    Logger.info('Express loaded');
+
+    if (config.env !== config.app.stage.test) {
+      await mongooseLoader()
+      Logger.info('DB Loaded and connected!')
+
+      app.listen(config.port, () => {
+        Logger.info(`
+        ####################################
+        #  Server listening on port: ${config.port}  #
+        ####################################
+        `)
+      })
+    }
+
+  } catch (error) {
+    Logger.error(error.message)
+    process.exit(1)
+  }
 }
 
-startServer()
+startServer(app)
+
+export default app
