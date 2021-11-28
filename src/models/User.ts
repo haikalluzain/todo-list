5942021132a498aaa3006a69e2ce9d2e11586138
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-11-28 11:49:33 +0700

    ♻️ User.ts: refactored user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
-import { Document, model, Schema } from 'mongoose'
+import { Document, Model, model, Schema } from 'mongoose'
 import { IUser } from '@interfaces'
 import { v4 } from 'uuid'
+import { compareHash } from '@utils/password'
 
 const UserSchema = new Schema(
   {
@@ -27,11 +28,36 @@ const UserSchema = new Schema(
   },
   { timestamps: {} }
 )
-
-interface UserDocument extends IUser, Document {
+interface IUserDocument extends IUser, Document {
   _id: string
+  isPasswordMatch: (password: string) => Promise<boolean>
 }
 
-const UserModel = model<UserDocument>("User", UserSchema)
+interface IUserModel extends Model<IUserDocument> {
+  isEmailTaken: (email: string, excludeId?: string) => Promise<boolean>
+}
+
+/**
+ * Check if the password match
+ * @param password 
+ * @returns {Promise<boolean>}
+ */
+UserSchema.methods.isPasswordMatch = async function (this: IUserDocument, password: string): Promise<boolean> {
+  const user = this
+  return await compareHash(password, user.password)
+}
+
+/**
+ * Check if email is already registered
+ * @param {string} email 
+ * @param {string} excludeId 
+ * @returns {Promise<boolean>}
+ */
+ UserSchema.statics.isEmailTaken = async function (this: IUserModel, email: string, excludeId?: string): Promise<boolean> {
+  const user = await this.findOne({ email, _id: { $ne: excludeId } })
+  return !!user
+}
+
+const UserModel = model<IUserDocument, IUser & IUserModel>("User", UserSchema)
 
 export default UserModel;
