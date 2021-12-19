a768fbf292ef6ee350cc9502e3178a5f352d0c20
Haikal F Luzain <haikalfikriluzain@gmail.com>
2021-12-19 14:57:49 +0700

    📝 Added comments

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -2,6 +2,11 @@ import bcrypt from 'bcrypt'
 
 const saltRounds = 10
 
+/**
+ * Generate hash from sting
+ * @param text {string}
+ * @return string
+ */
 export const generateHash = (text: string) => {
   return new Promise((resolve, reject) =>
     bcrypt.hash(text, saltRounds, (err, hash) => {
@@ -11,6 +16,11 @@ export const generateHash = (text: string) => {
   ) as Promise<string>
 }
 
+/**
+ * Compare the actual password text with the hashed password
+ * @param text {string}
+ * @param hash {string}
+ */
 export const compareHash = (text: string, hash: string) => {
   return bcrypt.compare(text, hash)
 }
